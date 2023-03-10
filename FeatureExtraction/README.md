d3abccabeff33fba533be09c07c0b9f2e8065be4
Isaac Garza <42215660+isaacgs99@users.noreply.github.com>
2023-03-10 11:42:47 -0600

    Add files via upload
    new project

---
+++ FeatureExtraction/README.md
@@ -0,0 +1,13 @@
+# FeatureExtraction
+
+Este es el módulo encargado de Feature Extraction para el proyecto de reconocimiento facial y verificacion.
+
+Recuerden que para poder compilar y correr el programa deben de seguir los siguientes pasos:
+
+1) mkdir build (En caso de que no exista)
+2) cd build
+3) cmake ..
+4) make
+5) ./my_project
+
+:)
---
+++ FeatureExtraction/build/CMakeFiles/my_project.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/rojo/Documentos/Git/FeatureExtraction/source/lbp.cpp" "/home/rojo/Documentos/Git/FeatureExtraction/build/CMakeFiles/my_project.dir/source/lbp.cpp.o"
+  "/home/rojo/Documentos/Git/FeatureExtraction/source/main.cpp" "/home/rojo/Documentos/Git/FeatureExtraction/build/CMakeFiles/my_project.dir/source/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/rojo/installation/OpenCV-master/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
